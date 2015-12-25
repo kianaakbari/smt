7b36ab41b6558a932110d9fcefb637f09ca34b26
kianaakbari <kianaakbari@yahoo.com>
2015-12-26 00:21:54 +0330

    Create ibm1.py

---
+++ ibm1.py
@@ -0,0 +1,117 @@
+#! /usr/bin/env python
+# coding:utf-8
+
+import decimal
+import sqlite3
+
+def mkcorpus(sentences):
+    return [(es.split(), fs.split()) for (es, fs) in sentences]
+
+def _train(corpus, loop_count=1000):
+    f_keys = set()
+    for (es, fs) in corpus:
+        for f in fs:
+            f_keys.add(f)
+
+    uniformPro = 1.0/len(f_keys)    #uniform probability initialization
+    db = sqlite3.connect('mydb.db')
+    cursor = db.cursor()
+    cursor.execute('''create table if not exists t(e text, f text, val real, primary key (e, f))''')  
+    cursor.execute('''create table if not exists count(e text, f text, val real, primary key (e, f))''')
+    cursor.execute('''create table if not exists total(f text unique, val real)''')
+    cursor.execute('''create table if not exists s_total(e text unique, val real)''')
+    for (es, fs) in corpus:
+        for e in es:
+            s_total = 0.0
+            for f in fs:
+                cursor.execute('''select val from t where e=? and f=?''',(e,f))
+                l = list(cursor)
+                if l==[]:
+                    t = uniformPro
+                else:   
+                    t = l[0][0]
+                s_total += t
+            try:
+                cursor.execute('''insert into s_total values(?, ?)''', (e, s_total))
+            except:
+                cursor.execute('''update s_total set val=? where e=?''', (s_total, e))
+                    
+        for e in es:
+            cursor.execute('''select val from s_total where e=?''', (e,))
+            l = list(cursor)
+            s_total = l[0][0]
+            for f in fs:
+                cursor.execute('''select val from total where f=?''', (f,))
+                l = list(cursor)
+                if l==[]:
+                    total = 0.0
+                else:
+                    total = l[0][0]
+                cursor.execute('''select val from count where e=? and f=?''', (e,f))
+                l = list(cursor)
+                if l==[]:
+                    count = 0.0
+                else:
+                    count = l[0][0]
+                cursor.execute('''select val from t where e=? and f=?''',(e,f))
+                l = list(cursor)
+                if l==[]:
+                    t =uniformPro
+                else:
+                    t = l[0][0]
+                        
+                count += t / s_total
+                total += t / s_total
+
+                try:
+                    cursor.execute('''insert into count values(?,?,?)''',(e, f, count))
+                except:
+                    cursor.execute('''update count set val=? where e=? and f=?''',(count, e, f))
+                try:
+                    cursor.execute('''insert into total values(?,?)''',(f, total))
+                except:
+                    cursor.execute('''update total set val=? where f=?''',(total, f))
+
+    c1 = db.cursor()
+    c1.execute('''select * from count''')
+    for row in c1:
+        cursor.execute('''select val from total where f=?''',(row[1],))
+        l1 = list(cursor)
+        total = l1[0][0]
+        try:
+            cursor.execute('''insert into t values(?,?,?)''', (row[0], row[1], row[2]/total))
+        except:
+            cursor.execute('''update t set val = ? where e=? and f=?''', (row[0], row[1], row[2]/total))
+    cursor.execute('''select * from t''')
+                     
+    cursor.execute('''create table if not exists myDict(englishWord text, persianWord text, val real)''')
+    cursor.execute('''insert into myDict select * from t order by e asc, val desc''')
+    cursor.execute('''drop table count''')
+    cursor.execute('''drop table total''')
+    cursor.execute('''drop table s_total''')
+    cursor.execute('''drop table t''')
+    
+    cursor.execute('''select * from myDict limit 50''')
+    
+
+    for row in cursor:
+        print("{} {}\t{}".format(row[0], row[1], row[2]))
+
+#
+    cursor.execute('''drop table myDict''')
+#    
+    db.commit()
+    db.close()
+    
+    
+def train(bitext):
+    corpus = mkcorpus(bitext)
+    return _train(corpus)
+
+if __name__ == '__main__':
+    es = open("Mizan_En.txt")
+    fs = open("Mizan_Fa.txt", encoding="utf8")
+    bitext = list(zip(es, fs))
+    train(bitext)
+
+    
